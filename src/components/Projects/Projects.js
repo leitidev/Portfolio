ab2922d00502a77673d52da84af3f779af52477a
leitidev <leiticontato@hotmail.com>
2024-10-12 17:38:27 -0300

    Database project implemented

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import lung from "../../Assets/Projects/lung.png";
+import data from "../../Assets/Projects/data.png";
 import Particle from "../Particle";
 import ProjectCard from "./ProjectCards";
 
@@ -36,6 +37,17 @@ function Projects() {
         {/* Database Section */}
         <h2 className="category-heading">  <strong className="purple">Database Projects</strong></h2>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={data}
+              isBlog={false}
+              title="Event Participation Database System"
+              description="
+This project is a relational database for managing event participation, tracking participants and events efficiently. It features participant and event management, auditing, and trigger-based logging to ensure data integrity."
+              ghLink="https://github.com/leitidev/database-management-system"
+            /*demoLink="https://plant49-ai.herokuapp.com/"*/
+            />
+          </Col>
 
         </Row>
 
